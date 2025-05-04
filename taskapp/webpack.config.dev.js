32f84ad7802911881a09950510c5e910e5202f73
Anthony <anthonyzhdong@gmail.com>
2025-05-05 10:31:02 +1200

    running webpack config using npm start

--- taskapp/webpack.config.dev.js
+++ taskapp/webpack.config.dev.js
@@ -1,14 +1,15 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = "development";
+
 
 module.exports = {
     mode: 'development',
     target: 'web',
-    devTool: 'cheaper-module-source-map',
-    entry: './public/index',
+    devtool: 'cheap-module-source-map',
+    entry: '/src/index',
     output: {
         path: path.resolve(__dirname,"build"),
         publicPath:'/',
