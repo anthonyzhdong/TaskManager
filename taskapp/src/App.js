2fea399f6f7df93a782fe95052a4a472fc224850
Anthony <anthonyzhdong@gmail.com>
2025-05-05 11:03:13 +1200

    basic header working

--- taskapp/src/App.js
+++ taskapp/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Routes, Route} from 'react-router-dom'
 import HomePage from './components/home/Home';
 import './App.css';
+import Header from './components/Header';
 import AboutPage from './components/about/About';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header/>
         <Routes>
           <Route path = "/" element= {<HomePage/>} />
         </Routes>
