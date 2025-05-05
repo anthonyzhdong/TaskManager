f6f757a035179b93c42769a7a5abfa4b382209ed
Anthony <anthonyzhdong@gmail.com>
2025-05-05 15:00:00 +1200

    form input

--- taskapp/src/components/tasks/TaskPage.js
+++ taskapp/src/components/tasks/TaskPage.js
@@ -1,13 +1,37 @@
 import React from 'react';
 
-const TaskPage = ()  => (
-    <div>
-        <h1> Task </h1>
+class TaskPage extends React.Component{
+
+    state = {
+        task:{
+            title: ""
+        }
+    };
+
+    
+
+    handleChange = (event) =>{
+        const task = {...this.state.task, title: event.target.value};
+        this.setState({task});
+    }
+    handleSubmit = (event) => {
+        event.preventDefault();
+        alert(this.state.task.title);
+    }
+
+     render(){
+        return (
+            <form onSubmit = {this.handleSubmit}>
+                <h2>Task</h2>
+                <h3>Add Task</h3>
+                <input type = "text" onChange={this.handleChange} value = {this.state.task.title} />
+                <input type = "submit" value = "Save"/>
+            </form>
+
+        );
+     }
 
 
-    </div>
 
-
-
-)
+}
 export default TaskPage;
