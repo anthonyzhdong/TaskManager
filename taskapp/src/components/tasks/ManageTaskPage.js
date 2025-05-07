7ffb82dbaf0ba1babce2c921258478da3c4d4999
Anthony <anthonyzhdong@gmail.com>
2025-05-07 16:09:29 +1200

    shows preexisting data in form

--- taskapp/src/components/tasks/ManageTaskPage.js
+++ taskapp/src/components/tasks/ManageTaskPage.js
@@ -1,36 +1,51 @@
-import React ,{useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import TaskForm from './TaskForm.jsx';
 import { newTask } from "../../../tools/mockData.js"
 import { useDispatch, useSelector } from "react-redux";
 import { loadAuthors } from "../../redux/actions/authorActions";
+import { useNavigate, useParams } from "react-router-dom";
+import { loadTasks, saveTask } from "../../redux/actions/taskActions";
 
-export default function ManageTaskPage(){
+
+export default function ManageTaskPage() {
     const authors = useSelector(state => state.authors);
+    const tasks = useSelector(state => state.tasks);
     const dispatch = useDispatch();
     const [task, setTask] = useState(newTask);
-    
+    const { slug } = useParams();
 
-    useEffect(()=>{
-        if(authors.length === 0){
+    useEffect(() => {
+        if (tasks.length === 0) {
+            dispatch(loadTasks()).catch(error => {
+                alert("loading tasks failed" + error);
+            });
+        } else if (slug) {
+            setTask(tasks.find(task => task.slug === slug) || newTask);
+        }
+    }, [tasks, slug]);
+
+
+    useEffect(() => {
+        if (authors.length === 0) {
             dispatch(loadAuthors()).catch(error => {
                 alert("loading authors failed" + error);
             });
         }
-    },[authors]);
+    }, [authors]);
 
-    function handleChange(event){
-        const {name, value} = event.target;
+    function handleChange(event) {
+        const { name, value } = event.target;
         setTask(prevTask => ({
             ...prevTask,
-            [name]: name === "authorId" ? parseInt(value,10) : value
+            [name]: name === "authorId" ? parseInt(value, 10) : value
         }));
     }
 
-    return (<TaskForm 
-    authors = {authors}
-    task = {task}
-    onChange = {handleChange}
-    onSave = {() =>{}}
+    return (<TaskForm
+        authors={authors}
+        task={task}
+        onChange={handleChange}
+        onSave={() => { }}
     />
     );
 }
