c43ee5cc199b8e39effd02d542b9e22d058bdbe4
Anthony <anthonyzhdong@gmail.com>
2025-05-08 15:51:00 +1200

    fixed up title

--- taskapp/src/components/tasks/TaskList.jsx
+++ taskapp/src/components/tasks/TaskList.jsx
@@ -18,7 +18,7 @@ const TaskList = ({ tasks }) => (
         return (
           <tr key={task.id}>
             {/* <td>${task.amount ? task.amount.toFixed(2) : "0.00"}</td> */}
-            {task.title}
+            <td>{task.title}</td>
             <td>{task.categoryName}</td>
             <td>{task.description}</td>
             <td>{task.date ? new Date(task.date).toLocaleDateString() : "Not set"}</td>
