9b3858e44e341dbd6a492412b19d5fdd6e621589
Anthony <anthonyzhdong@gmail.com>
2025-05-12 09:26:51 +1200

    adjusted home.jsx

--- taskapp/src/components/home/Home.jsx
+++ taskapp/src/components/home/Home.jsx
@@ -4,9 +4,6 @@ import { Link } from "react-router-dom";
 const HomePage = ()  => (
     <div>
         <h1> Home Title </h1>
-        <Link to="about" className="link">
-            Learn more
-        </Link>
 
 
     </div>
